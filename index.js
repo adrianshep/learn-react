dd66232f63eaa6d0e0861b1e8ae51307fbe0935b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-04-28 21:26:03 -0400

    1-ReactDOM & JSX - index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// JSX
+ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
+
+// ReactDOM.render(WHAT DO I WANT TO RENDER, WHERE DO I WANT TO RENDER IT)

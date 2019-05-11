7e2309b4445c3b4b51467cb5417575f02ba05b20
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-05-11 13:20:15 -0400

    lesson 16 - creating ToDoItem component

---
+++ 16-todo-app-phase2/ToDoItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ToDoItem() {
+  return (
+    return (
+      <div>
+        <input type="checkbox" />
+        <p>Placholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placholder text here</p>
+      </div>
+  )
+}
+
+export default ToDoItem;

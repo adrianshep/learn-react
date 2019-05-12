7c3a735d3ddcc9a2af9ce7d1e0a2b357dba1589f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-05-12 17:15:39 -0400

    lesson 20 - props and styling practice

---
+++ 20-props-styling-practice/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Joke from "./Joke";
+
+function App(props) {
+  return (
+    <div>
+      <Joke
+        question=""
+        punchLine=""
+      />
+      <Joke
+        question=""
+        punchLine=""
+      />
+      <Joke
+        question=""
+        punchLine=""
+      />
+      <Joke
+        question=""
+        punchLine=""
+      />
+      <Joke
+        question=""
+        punchLine=""
+      />
+    </div>
+  )
+}
+
+export default App;
